e881641cd1d4990f8280aa0c8d09cd209e245c61
Wojciech Tomaszewski <wojtekt@laptop-Wojciech.local>
2024-07-27 10:04:12 +0200

    add __repr__ to leaf node

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -10,12 +10,12 @@ class HTMLNode:
   def to_html(self):
     raise NotImplementedError
 
-  def props_to_html(self):
+  def props_to_html(self) -> str:
     if not self.props:
       return ""
     return reduce(lambda x, y: x + f' {y[0]}="{y[1]}"', self.props.items(), "")
 
-  def __repr__(self):
+  def __repr__(self) -> str:
     return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
   
 
@@ -23,12 +23,15 @@ class LeafNode(HTMLNode):
   def __init__(self, tag=None, value=None, props=None):
     super().__init__(tag=tag, value=value, props=props)
 
-  def to_html(self):
+  def to_html(self) -> str:
     if not self.value:
       raise ValueError("leaf node must have a value")
     if self.tag:
       return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
     return self.value
+  
+  def __repr__(self) -> str:
+    return f"LeafNode({self.tag}, {self.value}, {self.props})"
 
 
 if __name__ == "__main__":
