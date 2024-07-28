5649be655abf6c7b90aa09eab1f399a8450cf05c
Wojciech Tomaszewski <wojciech.tomaszewski1@ibm.com>
2024-07-28 15:19:17 +0200

    fix removed pprint

--- src/inline.py
+++ src/inline.py
@@ -117,4 +117,4 @@ def text_to_textnodes(text):
 if __name__ == "__main__":
   text = "This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
   nodes = text_to_textnodes(text)
-  pprint(nodes)
+  print(nodes)
