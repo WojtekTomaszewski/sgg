797f0bca0b32f0483ec012351172ba7703d1fb8d
Wojciech Tomaszewski <wojciech.tomaszewski1@ibm.com>
2024-07-28 20:13:52 +0200

    copy static file to public repo

---
+++ src/copystatic.py
@@ -0,0 +1,22 @@
+import os
+import shutil
+
+def copy_static(src_path, dst_path):
+  if not os.path.exists(dst_path):
+    os.mkdir(dst_path)
+
+  listdir = os.listdir(src_path)
+  if len(listdir) == 0:
+    return
+  
+  for entry in listdir:
+    src = os.path.join(src_path, entry)
+    dst = os.path.join(dst_path, entry)
+
+    if os.path.isfile(src):
+      print(f"Copy file {src}")
+      shutil.copy(src, dst)
+
+    if os.path.isdir(src):
+      # os.mkdir(dst)
+      copy_static(src, dst)
