143861d0490120145e194c69e194616c2e0bf35f
Wojciech Tomaszewski <wojciech.tomaszewski1@ibm.com>
2024-07-28 20:14:51 +0200

    run generation

--- src/main.py
+++ src/main.py
@@ -1,29 +1,8 @@
-import os
-import os.path
-import shutil
-
-def copy_static(src_path, dst_path):
-  if not os.path.exists(dst_path):
-    os.mkdir(dst_path)
-
-  
-  listdir = os.listdir(src_path)
-  if len(listdir) == 0:
-    return
-  
-  for entry in listdir:
-    src = os.path.join(src_path, entry)
-    dst = os.path.join(dst_path, entry)
-
-    if os.path.isfile(src):
-      print(f"Copy file {src}")
-      shutil.copy(src, dst)
-
-    if os.path.isdir(src):
-      # os.mkdir(dst)
-      copy_static(src, dst)
+from copystatic import copy_static
+from generate import generate_page, generate_page_recursive
 
 if __name__ == "__main__":
   copy_static("static", "public")
+  generate_page_recursive("content", "template.html", "public")
 
 
