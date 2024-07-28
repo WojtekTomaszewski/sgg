e4fdda6978e074262aeb4f80015e15c74135b9f5
Wojciech Tomaszewski <wojciech.tomaszewski1@ibm.com>
2024-07-28 19:08:09 +0200

    implement markdown parsing

--- src/block.py
+++ src/block.py
@@ -1,5 +1,19 @@
+import re
 
-def markdown_to_blocks(markdown):
+from pprint import pprint
+
+from htmlnode import ParentNode, LeafNode
+from inline import text_to_textnodes
+from textnode import TextNode, text_node_to_html_node
+
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_olist = "ordered_list"
+block_type_ulist = "unordered_list"
+
+def markdown_to_blocks(markdown) -> list[str]:
   splits = markdown.split("\n\n")
 
   blocks = []
@@ -11,16 +25,139 @@ def markdown_to_blocks(markdown):
   return blocks
 
 
-if __name__ == "__main__":
-  markdown = '''
-# This is a heading
+def block_to_block_type(block) -> str:
+  if re.match(r"^#{1,6}\s{1}\w+", block):
+    return block_type_heading
+  
+  lines = block.split("\n")
 
-This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+  if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+    return block_type_code
+  
+  if block.startswith(">"):
+    for line in lines:
+      if not line.startswith(">"):
+        return block_type_paragraph
+    return block_type_quote
+  
+  if block.startswith("* "):
+    for line in lines:
+      if not line.startswith("* "):
+        return block_type_paragraph
+    return block_type_ulist
+  
+  if block.startswith("- "):
+    for line in lines:
+      if not line.startswith("- "):
+        return block_type_paragraph
+    return block_type_ulist
+  
+  if block.startswith("1. "):
+    i = 1
+    for line in lines:
+      if not line.startswith(f"{i}. "):
+        return block_type_paragraph
+      i += 1
+    return block_type_olist
+  
+  return block_type_paragraph
 
-* This is the first list item in a list block
-* This is a list item
-* This is another list item
-'''
+
+def markdown_to_html_node(markdown):
   blocks = markdown_to_blocks(markdown)
-  print(len(blocks))
-  print(markdown_to_blocks(markdown))
+
+  div = ParentNode("div", [])
+
+  for block in blocks:
+    type = block_to_block_type(block)
+
+    if type == block_type_paragraph:
+      div.children.append(paragraph_block_to_node(block))
+      continue
+
+    if type == block_type_heading:
+      div.children.append(heading_block_to_node(block))
+      continue
+
+    if type == block_type_code:
+      div.children.append(code_block_to_node(block))
+      continue
+
+    if type == block_type_quote:
+      div.children.append(quote_block_to_node(block))
+      continue
+
+    if type == block_type_olist:
+      div.children.append(olist_block_to_node(block))
+      continue
+
+    if type == block_type_ulist:
+      div.children.append(ulist_block_to_node(block))
+      continue
+  return div
+
+
+def paragraph_block_to_node(block):
+  inline_nodes = text_to_textnodes(block)
+  return ParentNode("p", text_to_children(inline_nodes))
+
+def heading_block_to_node(block):
+  hashes, value = block.split(" ", maxsplit=1)
+  inline_nodes = text_to_textnodes(value)
+  return ParentNode(f"h{len(hashes)}", text_to_children(inline_nodes))
+
+def code_block_to_node(block):
+  value = block.strip("```")
+  inline_nodes = text_to_textnodes(value)
+  code_node = ParentNode("code", text_to_children(inline_nodes))
+  pre_node = ParentNode("pre", [code_node])
+  return pre_node
+
+def quote_block_to_node(block):
+  value_lines = block.replace("> ", "")
+  inline_nodes = text_to_textnodes(value_lines)
+  return ParentNode("blockquote", text_to_children(inline_nodes))
+
+def olist_block_to_node(block):
+  value_lines = block.split("\n")
+  value_lines_without_number = list(map(lambda x: x.split(" ", maxsplit=1)[1], value_lines))
+  inline_nodes = []
+  for value_line in value_lines_without_number:
+    inline_nodes.extend(text_to_textnodes(value_line))
+  li_nodes = []
+  for inline_node in inline_nodes:
+    li_nodes.append(ParentNode("li", [text_node_to_html_node(inline_node)]))
+  return ParentNode("ol", li_nodes)
+
+def ulist_block_to_node(block):
+  value_lines = block.split("\n")
+  value_lines_without_number = list(map(lambda x: x.split(" ", maxsplit=1)[1], value_lines))
+  inline_nodes = []
+  for value_line in value_lines_without_number:
+    inline_nodes.extend(text_to_textnodes(value_line))
+  li_nodes = []
+  for inline_node in inline_nodes:
+    li_nodes.append(ParentNode("li", [text_node_to_html_node(inline_node)]))
+  return ParentNode("ul", li_nodes)
+
+def text_to_children(text_nodes) -> list[LeafNode]:
+  return list(map(text_node_to_html_node, text_nodes))
+
+if __name__ == "__main__":
+#   markdown = '''
+# # This is a heading
+
+# This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+
+# * This is the first list item in a list block
+# * This is a list item
+# * This is another list item
+# '''
+  markdown = '''
+1. one
+2. two
+'''
+  node = markdown_to_html_node(markdown)
+  pprint(markdown_to_html_node(markdown))
+
+  pprint(node.to_html())
--- src/test_block.py
+++ src/test_block.py
@@ -1,6 +1,19 @@
 import unittest
 
-from block import markdown_to_blocks
+from block import (
+  markdown_to_blocks,
+  block_to_block_type,
+  markdown_to_html_node,
+  block_type_code,
+  block_type_heading,
+  block_type_olist,
+  block_type_paragraph,
+  block_type_quote,
+  block_type_ulist
+)
+
+from htmlnode import ParentNode, LeafNode
+from textnode import TextNode
 
 class TestMarkdownToBlocks(unittest.TestCase):
   def test_outputs(self):
@@ -20,5 +33,110 @@ This is a paragraph of text. It has some **bold** and *italic* words inside of i
     self.assertEqual(len(blocks), 3)
     self.assertEqual(blocks[0], expected[0])
 
+
+class TestBlockToBlockType(unittest.TestCase):
+  def test_heading(self):
+    block = '# This is a heading'
+    block_type = block_to_block_type(block)
+    self.assertEqual(block_type, block_type_heading)
+
+  def test_paragraph(self):
+    block = 'This is a paragraph of text. It has some **bold** and *italic* words inside of it.'
+    block_type = block_to_block_type(block)
+    self.assertEqual(block_type, block_type_paragraph)
+
+  def test_quote(self):
+    block = '> This is the first list item in a list block\n> This is a list item\n> This is another list item'
+    block_type = block_to_block_type(block)
+    self.assertEqual(block_type, block_type_quote)
+
+  def test_ulist_hyphen(self):
+    block = '- This is the first list item in a list block\n- This is a list item\n- This is another list item'
+    block_type = block_to_block_type(block)
+    self.assertEqual(block_type, block_type_ulist)
+
+  def test_ulist_star(self):
+    block = '* This is the first list item in a list block\n* This is a list item\n* This is another list item'
+    block_type = block_to_block_type(block)
+    self.assertEqual(block_type, block_type_ulist)
+
+  def test_olist(self):
+    block = '1. This is the first list item in a list block\n2. This is a list item\n3. This is another list item'
+    block_type = block_to_block_type(block)
+    self.assertEqual(block_type, block_type_olist)
+
+
+class TestMarkdownToHTMLNode(unittest.TestCase):
+  def test_paragraph(self):
+    markdown = "This is a paragraph of text. It has some **bold** and *italic* words inside of it."
+    node = markdown_to_html_node(markdown)
+    html = node.to_html()
+    self.assertEqual(
+      html,
+      '<div><p>This is a paragraph of text. It has some bold and <i>italic</i> words inside of it.</p></div>'
+    )
+
+  def test_heading(self):
+    markdown = "# My Heading"
+    node = markdown_to_html_node(markdown)
+    html = node.to_html()
+    self.assertEqual(
+      html,
+      '<div><h1>My Heading</h1></div>'
+    )
+
+  def test_code(self):
+    markdown = '''
+```
+def f(x):
+  print(x)
+```
+'''
+    node = markdown_to_html_node(markdown)
+    html = node.to_html()
+    self.assertEqual(
+      html,
+      '<div><pre><code>\ndef f(x):\n  print(x)\n</code></pre></div>'
+    )
+
+  def test_quote(self):
+    markdown = '''
+> To be
+> or not to be!
+'''
+    node = markdown_to_html_node(markdown)
+    html = node.to_html()
+    self.assertEqual(
+      html,
+      '<div><blockquote>To be\nor not to be!</blockquote></div>'
+    )
+
+  def test_olist(self):
+    markdown = '''
+1. one
+2. two
+'''
+    node = markdown_to_html_node(markdown)
+    html = node.to_html()
+    self.assertEqual(
+      html,
+      '<div><ol><li>one</li><li>two</li></ol></div>'
+    )
+
+  def test_ulist(self):
+    markdown = '''
+* one
+* two
+'''
+    node = markdown_to_html_node(markdown)
+    html = node.to_html()
+    self.assertEqual(
+      html,
+      '<div><ul><li>one</li><li>two</li></ul></div>'
+    )
+
 if __name__ == "__main__":
   unittest.main()
+
+
+  
